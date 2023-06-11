48dcd27773944d5c038af5a4da9944caf9e115a4
realjcwkm <realjcwkm@gmail.com>
2023-06-11 10:46:07 -0400

    add find employee by ID route

--- src/controllers/employeeController.ts
+++ src/controllers/employeeController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { findAllEmployees } from '../usecases/employees/findAllEmployees';
 import { insertEmployee } from '../usecases/employees/insertEmployee';
 import { updateEmployee as employeeUpdate } from '../usecases/employees/updateEmployee';
+import { findEmployeeById } from '../usecases/employees/findEmployeeById';
 
 async function getEmployees(req: Request, res: Response) {
 	const employees = await findAllEmployees();
@@ -45,4 +46,17 @@ async function updateEmployee(req: Request, res: Response){
 	return res.status(404).send('Erro ao atualizar');
 }
 
-export { getEmployees, createEmployee, updateEmployee };
+async function getEmployee(req: Request, res: Response) {
+	const idEmployee = Number(req.params.id);
+
+	const employee = await findEmployeeById(idEmployee);
+
+	if (employee) return res.send(employee);
+
+	return res.status(404).send({
+		Error: 'Unable to find employee by id',
+		status: 404,
+	});
+}
+
+export { getEmployees, createEmployee, updateEmployee, getEmployee };
--- src/routes/employees.ts
+++ src/routes/employees.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { createEmployee, getEmployees, updateEmployee } from '../controllers/employeeController';
+import { createEmployee, getEmployees, updateEmployee, getEmployee } from '../controllers/employeeController';
 
 const employeesRouter = Router();
 
 employeesRouter.get('/employees', getEmployees);
-//employeesRouter.get('/employee/:id', getEmployee);
+employeesRouter.get('/employee/:id', getEmployee);
 employeesRouter.post('/employee', createEmployee);
 employeesRouter.put('/employee/:id', updateEmployee);
 //Adicionar as outras rotas posteriormente
