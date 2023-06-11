3d3ae65023462bfc1364ed33f0b8a06416f12a95
realjcwkm <realjcwkm@gmail.com>
2023-06-10 23:51:10 -0400

    add employee endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { clientsRouter } from './clients';
+import { employeesRouter } from './employees';
 
 const router = Router();
 
 router.use(clientsRouter);
+router.use(employeesRouter);
 
 export { router };
